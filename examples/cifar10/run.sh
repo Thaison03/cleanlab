119122110566a493941eadcc09efe4a3ccf79652
litsh <litsh@shanghaitech.edu.cn>
2021-09-21 20:55:12 +0800

    add data

---
+++ examples/cifar10/run.sh
@@ -0,0 +1,5 @@
+python3 cifar10_train_crossval.py \
+    -a resnet50 --gpu 0 --cvn 4 --cv 0  \
+    --train-labels /home/thaison/AI_Security/cleanlab/confidentlearning-reproduce/cifar10/cifar10_noisy_labels/cifar10_noisy_labels__frac_zero_noise_rates__0.4__noise_amount__0.4.json /home/thaison/AI_Security/cleanlab/examples/cifar10/dataset     
+     
+
